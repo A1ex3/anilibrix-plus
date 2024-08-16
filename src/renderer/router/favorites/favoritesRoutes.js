c92699d24744e4cd59f0e2b420bbfa17b952d9c0
A1ex3 <pakshin.2018@gmail.com>
2024-08-16 19:41:59 +0700

    feat(base layout): added "up" button when clicking on it the page will scroll upwards
    Pages on which this function is active: "Каталог", "Избранное" and release page.

--- src/renderer/router/favorites/favoritesRoutes.js
+++ src/renderer/router/favorites/favoritesRoutes.js
@@ -2,7 +2,7 @@ export default [
   {
     name: 'favorites',
     path: '/favorites',
-    meta: { layout: { show_scroll: true } },
+    meta: { layout: { show_scroll: true, scroll_to_top: true } },
     props: true,
     component: () => import('@views/favorites')
   }
